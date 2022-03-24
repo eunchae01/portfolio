8f51082067b4228976e65ff4ef2dec26c77c4c7d
201815069 <70687333+eunchae01@users.noreply.github.com>
2022-03-25 00:03:49 +0900

    contact페이지 테이블 추가

---
+++ MyPage/WebContent/js/console.js
@@ -0,0 +1,30 @@
+function url1(){
+    let btn = document.getElementById("urlBtn1")
+    let show = 0;
+
+    btn.onclick = function(){
+        if(show == 0){
+            document.getElementById("url1").style.display = "inline-block";
+            show++;
+        }else{
+            document.getElementById("url1").style.display = "none";
+            show--;
+        }
+    }
+}
+
+function url2(){
+    let btn = document.getElementById("urlBtn2")
+    let show = 0;
+
+    btn.onclick = function(){
+        if(show == 0){
+            document.getElementById("url2").style.display = "inline-block";
+            show++;
+            console.log(show);
+        }else{
+            document.getElementById("url2").style.display = "none";
+            show--;
+        }
+    }
+}
